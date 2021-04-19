66692405c668fd8da0b6617fcb5e4cf15fd56e90
younghoon <digitcomgisul@gmail.com>
2021-04-19 11:01:18 +0900

    lxml
    xpath사용
    2021년 4월 19일 기준 - 언론사명, 링크 스크래핑
    파일로 저장

--- section02-4.py
+++ section02-4.py
@@ -5,6 +5,8 @@
 import requests
 from lxml.html import fromstring, tostring
 
+file_path_name = 'd:/study_test/news.txt'
+
 def main():
     """
     네이버 메인 뉴스 스탠드 스크래핑 메인함수
@@ -19,14 +21,13 @@ def main():
     # 신문사 링크 딕셔너리 획득
     urls = scrape_news_list_page(response)
 
-    # 딕셔너리 확인
-    # print(urls)
+    # 파일에 쓰기
+    with open(file_path_name, 'a') as c:
+        for k in urls.keys():
+            l = urls.get(k)
+            news = k + " : " + l + "\n"
+            c.write(news)
 
-
-    # 결과 출력
-    for name, url in urls.items():
-        # url 출력
-        print(name)
 
 
 def scrape_news_list_page(response):
@@ -41,29 +42,48 @@ def scrape_news_list_page(response):
 
     # xpath 사용법
     # // 전체문서
-    for a in root.xpath('//ul[@class="api_list"]/li[@class="api_item"]/a[@class="api_link"]'):
+    # cont = root.xpath('//div[@id="NM_NEWSSTAND_VIEW_CONTAINER"]/div[@id="NM_NEWSSTAND_DEFAULT_THUMB"]/div[@class="_NM_UI_PAGE_CONTAINER"]')
+    # cont = root.xpath('//div[@class="thumb_box _NM_NEWSSTAND_THUMB _NM_NEWSSTAND_THUMB_press_valid"]')
+    # print("##########")
+    # print(cont)
+    # print("##########")
+    # for a in root.xpath('//ul[@class="api_list"]/li[@class="api_item"]/a[@class="api_link"]'):
+    for a in root.xpath('//div[@class="thumb_box _NM_NEWSSTAND_THUMB _NM_NEWSSTAND_THUMB_press_valid"]'):
+        # print(tostring(a, pretty_print=True))
 
         # a 구조 확인
         # print(a)
 
-        # a 문자열 출력
-        # print(tostring(a, pretty_print=True))
-
+        # dom에서 뉴스매체 이름, url을 가져옴
         name, url = extract_contents(a)
+
         # 딕셔너리 삽입
         urls[name] = url
 
-        return urls
+    return urls
+
 
 def extract_contents(dom):
-    # 링크 주소
-    link = dom.get("href")
+    name = dom.xpath('./a[@class="thumb"]/img')[0].get('alt')
+    # a_link = a.xpath('./div[@class="popup_wrap"]/a[@class="btn_popup"]')  btn_popup이 3개임
+    a_link = dom.xpath('./div[@class="popup_wrap"]/a[@data-clk="logo"]')[0]
+    url = a_link.get("href")
 
-    # 신문사 명
-    name = dom.xpath('./img')[0].get('alt')
-    # print(name)
+    # for b in a_link:
+    #     print(tostring(b, pretty_print=True))
 
-    return name, link
+    # print("##########")
+    # print(a_link)
+    # print(tostring(a_link, pretty_print=True))
+    # print("##########")
+    # print(url)
+    # print("##########")
+    # print(tostring(a_link, pretty_print=True))
+    # url = a_link.get("href")
+
+    # print(url)
+
+    return name, url
 
 
 
